baca3499b72e0758b4caf3577e9448e78e8b56f0
sam <sam@silver42.karoo.co.uk>
2016-08-28 00:45:26 +0100

    More attempting to work with hxcpp prime

--- extension/notifications/Notifications.hx
+++ extension/notifications/Notifications.hx
@@ -39,20 +39,20 @@ class Notifications {
 
 	#if android
 	private static inline var packageName:String = "com/samcodes/notifications/NotificationsExtension";
-	private static inline function bindJNI(jniMethod:String, jniSignature:String):Dynamic {
+	private static inline function bindJNI(jniMethod:String, jniSignature:String) {
 		return JNI.createStaticMethod(packageName, jniMethod, jniSignature);
 	}
-	private static var schedule_local_notification:Dynamic = bindJNI("scheduleLocalNotification", "(IILjava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Z)V");
-	private static var cancel_local_notification:Dynamic = bindJNI("cancelLocalNotification", "(I)V");
-	private static var cancel_local_notifications:Dynamic = bindJNI("cancelLocalNotifications", "()V");
-	private static var get_application_icon_badge_number:Dynamic = bindJNI("getApplicationIconBadgeNumber", "()I");
-	private static var set_application_icon_badge_number:Dynamic = bindJNI("setApplicationIconBadgeNumber", "(I)Z");
+	private static var schedule_local_notification = bindJNI("scheduleLocalNotification", "(IILjava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Z)V");
+	private static var cancel_local_notification = bindJNI("cancelLocalNotification", "(I)V");
+	private static var cancel_local_notifications = bindJNI("cancelLocalNotifications", "()V");
+	private static var get_application_icon_badge_number = bindJNI("getApplicationIconBadgeNumber", "()I");
+	private static var set_application_icon_badge_number = bindJNI("setApplicationIconBadgeNumber", "(I)Z");
 	#elseif ios
-	private static var schedule_local_notification:Dynamic = PrimeLoader.load("samcodesnotifications_schedule_local_notification", "iissssbv");
-	private static var cancel_local_notification:Dynamic = PrimeLoader.load("samcodesnotifications_cancel_local_notification", "iv");
-	private static var cancel_local_notifications:Dynamic = PrimeLoader.load("samcodesnotifications_cancel_local_notifications", "v");
-	private static var get_application_icon_badge_number:Dynamic = PrimeLoader.load("samcodesnotifications_get_application_icon_badge_number", "i");
-	private static var set_application_icon_badge_number:Dynamic = PrimeLoader.load("samcodesnotifications_set_application_icon_badge_number", "ib");
+	private static var schedule_local_notification = PrimeLoader.load("samcodesnotifications_schedule_local_notification", "iissssbv");
+	private static var cancel_local_notification = PrimeLoader.load("samcodesnotifications_cancel_local_notification", "iv");
+	private static var cancel_local_notifications = PrimeLoader.load("samcodesnotifications_cancel_local_notifications", "v");
+	private static var get_application_icon_badge_number = PrimeLoader.load("samcodesnotifications_get_application_icon_badge_number", "i");
+	private static var set_application_icon_badge_number = PrimeLoader.load("samcodesnotifications_set_application_icon_badge_number", "ib");
 	#end
 }
 #end
