40718a54c564c6f5cc7f29dfd0bcc98ef7e1e11b
sam <sam@silver42.karoo.co.uk>
2016-08-27 18:45:47 +0100

    Some iOS badging implementation

--- project/iphone/SamcodesNotifications.mm
+++ project/iphone/SamcodesNotifications.mm
@@ -2,6 +2,8 @@
 
 #import <UIKit/UIKit.h>
 
+#define kShouldNotificationIncrementBadgeKey @"kIncrementBadgeKey"
+
 @interface NotificationsController : NSObject<UIApplicationDelegate>
 @end
 
@@ -12,21 +14,28 @@
     [application registerForRemoteNotifications];
 }
 
-- (void)scheduleLocalNotification:(int)slot withTimeInterval:(int)timeInterval withTitle:(NSString*)title withBody:(NSString*)messageBody withAction:(NSString*)action
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
 {
-	// TODO this should go in didFinishLaunchingWithOptions?
+	// Request permissions to provide local notifications
+	UIRemoteNotificationType types = UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert;
 	if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)]) // iOS 8 and above
 	{
-		UIUserNotificationType types = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
-		UIUserNotificationSettings* mySettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
-		[[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
+		[[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:types categories:nil]];
 	}
 	else
 	{
-		UIRemoteNotificationType types = UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert;
 		[[UIApplication sharedApplication] registerForRemoteNotificationTypes:types];
 	}
-	
+}
+
+- (void)application:(UIApplication*)application didReceiveLocalNotification:(UILocalNotification*)notification
+{
+	// This should be triggered if the app is active in the foreground when a local notification is received
+	// Or if the user just launched the app from a notification
+}
+
+- (void)scheduleLocalNotification:(int)slot withTimeInterval:(int)timeInterval withTitle:(NSString*)title withBody:(NSString*)messageBody withAction:(NSString*)action
+{
 	UILocalNotification* notification = [[UILocalNotification alloc] init];
 	notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:timeInterval];
 	notification.timeZone = [NSTimeZone defaultTimeZone];
@@ -47,28 +56,30 @@
 	
 	notification.soundName = UILocalNotificationDefaultSoundName;
 	
-	//NSString* userDataId = [NSString stringWithFormat:@"%d", slot];
-	//notification.userdata
-	
 	[[UIApplication sharedApplication] scheduleLocalNotification:notification];
 }
 
 - (void)cancelLocalNotification:(int)slot
 {
-	// TODO grab list of active notifications
-	// TODO check userdata for id key
-	// TODO if matching, then grab and cancel it
-	//[[UIApplication sharedApplication] cancelLocalNotification:notification];
+	NSMutableArray* pendingNotifications = [[UIApplication sharedApplication] scheduledLocalNotifications];
+	if (pendingNotifications.count != 0)
+	{
+		for (UILocalNotification* notification in pendingNotifications)
+		{
+			// TODO check userdata for id key
+			// TODO if matching, then grab and cancel it, and recalculate local badge counts
+			//[[UIApplication sharedApplication] cancelLocalNotification:notification];
+			//if([getApplicationIconBadgeNumber] > 0)
+			//{
+			//	[recalculateLocalNotificationBadgeCounts:currentBadgeNumber--];
+			//}
+		}
+	}
 }
 
 - (void)cancelLocalNotifications
 {
 	[[UIApplication sharedApplication] cancelAllLocalNotifications];
-}
-
-- (void)application:(UIApplication*)application didReceiveLocalNotification:(UILocalNotification*)notification
-{
-    application.applicationIconBadgeNumber += 1; // TODO check if app is in background or foreground?
 }
 
 - (int)getApplicationIconBadgeNumber
@@ -79,6 +90,43 @@
 - (void)setApplicationIconBadgeNumber:(int)number
 {
 	[[UIApplication sharedApplication] setApplicationIconBadgeNumber:number];
+	[recalculateLocalNotificationBadgeCounts:number];
+}
+
+// Ensures local notifications set badge numbers correctly
+- (void)recalculateLocalNotificationBadgeCounts:(int)baseBadgeCount
+{
+	// Get a copy of all pending notifications
+	NSMutableArray* pendingNotifications = [[[UIApplication sharedApplication] scheduledLocalNotifications] mutableCopy];
+	
+	// Sort them by fire date
+	NSSortDescriptor* sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"fireDate" ascending:TRUE];
+	[pendingNotifications sortUsingDescriptors:[NSArray arrayWithObject:sortDescriptor]];
+	[sortDescriptor release];
+	
+	// Reschedule all the local notifications with updated badge numbers
+	if (pendingNotifications.count != 0)
+	{
+		[[UIApplication sharedApplication] cancelAllLocalNotifications];
+		NSUInteger badgeNumber = baseBadgeCount;
+		for (UILocalNotification* notification in pendingNotifications)
+		{
+			//TODO
+			//NSDictionary* userInfo = notification.userInfo;
+			//if([
+			bool shouldIncrementBadgeCount = true;
+			if(shouldIncrementBadgeCount)
+			{
+				notification.applicationIconBadgeNumber = badgeNumber++;
+			}
+			else
+			{
+				notification.applicationIconBadgeNumber = badgeNumber;
+			}
+			[[UIApplication sharedApplication] scheduleLocalNotification:notification];
+		}
+	}
+	[pendingNotifications release];
 }
 
 @end
