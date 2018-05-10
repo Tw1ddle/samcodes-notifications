6562346b2334f1bc748ba2977ed012f700e3bdd3
sam <sam@silver42.karoo.co.uk>
2018-05-10 23:03:16 +0100

    Rename Android's "ongoing" param, document the scheduleLocalNotification methods

--- extension/notifications/Notifications.hx
+++ extension/notifications/Notifications.hx
@@ -12,11 +12,32 @@ import extension.notifications.PrimeLoader;
 class Notifications {
 	// Note, keeping these methods separate since even the common parameters here serve pretty different purposes
 	// It would be better to wrap these methods if you use them a lot, as they might change in the future
+	// See the demo project here for usage examples: https://github.com/Tw1ddle/samcodes-notifications-demo
 	#if android
-	public static function scheduleLocalNotification(slot:Int, triggerAfterSecs:Float, titleText:String, subtitleText:String, messageBodyText:String, tickerText:String, incrementBadgeCount:Bool, ongoing:Bool):Void {
-		schedule_local_notification(slot, triggerAfterSecs, titleText, subtitleText, messageBodyText, tickerText, incrementBadgeCount, ongoing);
+	/**
+	   Schedules a local notification on Android
+	   @param	slot The slot index of the notification (you should define a set of values for these using an enum abstract)
+	   @param	triggerAfterSecs The number of seconds until the notification will be presented
+	   @param	titleText The title text shown in the notification
+	   @param	subtitleText The subtitle text shown in the notification
+	   @param	messageBodyText The body/message text shown in the notification
+	   @param	tickerText The ticker/status bar text shown in the notification (likely to be what's read by accessibility services too)
+	   @param	incrementBadgeCount Whether to increment the application badge count when the notification is triggered
+	   @param	isOngoing Whether the notification is the "ongoing" (persistent) notification type
+	**/
+	public static function scheduleLocalNotification(slot:Int, triggerAfterSecs:Float, titleText:String, subtitleText:String, messageBodyText:String, tickerText:String, incrementBadgeCount:Bool, isOngoing:Bool):Void {
+		schedule_local_notification(slot, triggerAfterSecs, titleText, subtitleText, messageBodyText, tickerText, incrementBadgeCount, isOngoing);
 	}
 	#elseif ios
+	/**
+	   Schedules a local notification on iOS
+	   @param	slot The slot index of the notification (you should define a set of values for these using an enum abstract)
+	   @param	triggerAfterSecs The number of seconds until the notification will be presented
+	   @param	titleText The title text shown in the notification
+	   @param	messageBodyText The body/message text shown in the notification
+	   @param	actionButtonText The text shown on the action button associated with the notification
+	   @param	incrementBadgeCount Whether to increment the application badge count when the notification is triggered
+	**/
 	public static function scheduleLocalNotification(slot:Int, triggerAfterSecs:Float, titleText:String, messageBodyText:String, actionButtonText:String, incrementBadgeCount:Bool):Void {
 		schedule_local_notification.call(slot, triggerAfterSecs, titleText, messageBodyText, actionButtonText, incrementBadgeCount);
 	}
